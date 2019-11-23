e595a6d972859150fc343ddf000a9b6166f2ce0a
Revital <revital@RevitalsMacBook.lan>
2019-11-23 18:21:14 +0200

    muzilator app example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { initialize, Endpoint } from '@muzilator/sdk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const init = async () => {
+  const platform = await initialize()
+  // await platform.loadLibrary('bubbles-vanilla-example', 'bubbles', 'primary')
+  await platform.loadLibrary('piano-keyboard', 'piano-keyboard', 'primary')
+  await platform.loadLibrary('midi-synth', 'synth')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  await platform.connectChannels(Endpoint('piano-keyboard', 'midi'), Endpoint('synth', 'midi'))
+}
+
+init()
